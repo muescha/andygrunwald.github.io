abec6c7f5b5b83fa549106ddcdd234541c064f8e
Andy Grunwald <andygrunwald@gmail.com>
2016-08-19 17:21:36 +0200

    Rebuilding the site - Fri 19 Aug 2016 17:21:36 CEST

--- publish.sh
+++ publish.sh
@@ -4,20 +4,28 @@
 # Basic script to publish the content of the static site generator to github.
 # The generated content will be our github pages.
 #
+# I know that git push --force is not the best trick
+# But hey, for this use case and the result of static html it works fine
+# We still keep the history of development in the master branch
+#
 echo -e "\033[0;32mDeploying updates to GitHub ...\033[0m"
 
 # Cleanup
-rm -rf docs || exit 0;
+rm -rf public || exit 0;
 
 # Build the project.
 hugo
 if [ $? -ne 0 ]; then echo "Could not generate the site"; exit 1; fi
 
-mv ./public ./docs
+cp CNAME ./public/
 
-cp CNAME ./docs/
+# Add changes to git by create a fresh git repo
+git init
 
-# Add changes to git.
+git config user.email "andygrunwald@gmail.com"
+git config user.name "Andy Grunwald"
+
+git remote add origin git@github.com:andygrunwald/andygrunwald.github.io.git
 git add -A
 
 # Commit changes.
@@ -28,7 +36,7 @@ fi
 git commit -m "$msg"
 
 # Push source to main line and gh-pages
-git push origin master
-if [ $? -ne 0 ]; then echo "Could not publish latest state to master"; exit 1; fi
+git push --force origin master
+if [ $? -ne 0 ]; then echo "Could not publish the site"; exit 1; fi
 
 echo -e "\033[0;32mDeploying updates to GitHub ... Done\033[0m"
